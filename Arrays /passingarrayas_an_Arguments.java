e2c4b40a3d61e9e2ae89122266f4b46f1c88a879
subhashadireddy <subhashadireddy@gmail.com>
2026-01-20 19:03:14 +0530

    Create passingarrayas_an_Arguments.java
    arrays can be passes as an argumwnts in the functions but through the reference , there will be the changes inn the main function

---
+++ Arrays /passingarrayas_an_Arguments.java
@@ -0,0 +1,18 @@
+
+public class passingarray_arguments {
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i] = marks[i]+1;
+        }
+    }
+    public static void main(String[] args){
+        int marks[] = {1,2,3};
+        update(marks);
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(marks[i]+" ");
+        }
+    }
+    
+}
