bec78bc6aa45fc186b38a9b8b85b41122a196d52
subhashadireddy <subhashadireddy@gmail.com>
2026-02-08 23:03:21 +0530

    Add Stringaddition.java for string concatenation example

---
+++ Strings /Stringaddition.java
@@ -0,0 +1,12 @@
+class GFG {
+ 
+    public static void main(String args[]) {
+      
+        // String Initialization
+        String s = "Geeks";
+       
+        // Use concat() method for string concatenation
+        s = s.concat("forGeeks");
+        System.out.println(s);
+    }
+}
