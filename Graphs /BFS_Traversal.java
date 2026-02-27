24ca1bfbdfdf9467450f0289e8d0c78567214bf0
subhashadireddy <subhashadireddy@gmail.com>
2026-02-27 15:14:45 +0530

    Create BFS_Traversal.java

---
+++ Graphs /BFS_Traversal.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class BFS {
+    static void BFSearch(int s,int n ,List<List<Integer>> g)
+    {
+        boolean[] visited = new boolean[n];
+        visited[s] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        while(!q.isEmpty())
+        {
+            int no = q.poll();
+            System.out.println(no);
+            for(int i : g.get(no))
+            {
+                if(!visited[i])
+                {
+                    visited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+         public static void main(String[] args){
+        int [][]a = {{0,1},{0,2},{0,3},{3,2},{1,3},{2,4},{3,4}};
+        List<List<Integer>> g = new ArrayList<>();
+        for(int i=0;i<5;i++)
+        {
+            g.add(new ArrayList<>());
+        }
+        for(int i=0;i<a.length;i++)
+        {
+                int x = a[i][0];
+                int y = a[i][1];
+                g.get(x).add(y);
+                g.get(y).add(x);
+        }
+        System.out.println(g);
+
+        BFSearch(0,5,g);
+}
+}
