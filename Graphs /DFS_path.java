ae064a13ecccad1c889a2411954c96bd3dd7bc5b
subhashadireddy <subhashadireddy@gmail.com>
2026-02-28 10:35:34 +0530

    Create DFS_path.java
    queue is declared in globally

---
+++ Graphs /DFS_path.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+public class DFS {
+
+    public static Queue<Integer> p = new LinkedList<>();
+    static void DFSearch(int s,  List<List<Integer>> g , boolean[] visited,int d) {
+        // System.out.println(s);
+        p.add(s);
+        visited[s] = true;
+        if(s==d)
+        {
+            System.out.println(p);
+        }
+        else{
+            for (int i : g.get(s)) {
+                if (!visited[i]) {
+                    DFSearch(i,g,visited,d);
+                }
+            }
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{0, 1}, {0, 2}, {0, 3}, {3, 2}, {1, 3}, {2, 4}, {3, 4}};
+        List<List<Integer>> g = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            g.add(new ArrayList<>());
+        }
+        for (int i = 0; i < a.length; i++) {
+            int x = a[i][0];
+            int y = a[i][1];
+            g.get(x).add(y);
+            g.get(y).add(x);
+        }
+        System.out.println(g);
+
+        boolean[] visited = new boolean[5];
+        DFSearch(0, g,visited,2);
+    }
+}
